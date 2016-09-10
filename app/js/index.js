f6f3b6ec0faf9b17f7de3899893ba7b9aaef9f9e
Benjamin Van Treese <vantreeseba@gmail.com>
2016-09-09 23:50:00 -0500

    Combine button and timer vtree

--- app/js/index.js
+++ app/js/index.js
@@ -1,20 +1,22 @@
 var most = require('most');
 var run = require('@cycle/most-run').default.run;
-var {makeDOMDriver, h1, div} = require('@cycle/dom');
+var { makeDOMDriver, h1, div } = require('@cycle/dom');
 var isolate = require('@cycle/isolate').default;
 
 var timer = require('./components/timer.js');
 var button = require('./components/button.js');
 
 function main(sources) {
-  var props = most.of({text: "hello"});
-  var testButton = button({props: props});
-  var buttonVTREE = testButton.DOM;
+  var props = most.of({ text: "hello" });
+  var testButton = button({ props: props });
+  var buttonVTree$ = testButton.DOM;
 
-  var timerVTree = timer().DOM;
+  var timerVTree$ = timer().DOM;
 
   var sinks = {
-    DOM: timerVTree 
+    DOM: most.combine((buttonVTree, timerVTree) => {
+      return div([buttonVTree, timerVTree])
+    }, buttonVTree$, timerVTree$)
   };
 
   return sinks;
